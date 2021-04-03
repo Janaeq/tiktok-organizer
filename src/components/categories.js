e39a0499bab610276832033344e0963796049558
Janae Quinones <janaemassa@gmail.com>
2021-04-03 15:45:23 -0400

    organize categories

---
+++ src/components/categories.js
@@ -0,0 +1,23 @@
+class Categories {
+    constructor() {
+        this.categories = []
+        this.adapter = new CategoryAdapter()
+        // this.bindEventListeners()
+        this.fetchAndLoadCategories()
+    }
+
+    fetchAndLoadCategories() {
+        // makes a call to backend API using getCategories method (defined in categories.js)
+        this.adapter.getCategories().then(categories => {
+            // pushes the categories from the API into the categores array defined above
+            categories.forEach(category => this.categories.push(category))
+        })
+        .then(() => {
+            this.renderCategories()
+        })
+    }
+    renderCategories() {
+        // show categories on DOM
+        console.log("manipulate DOM here")
+    }
+}
