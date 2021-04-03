a8e55e4df4c784372fb4e707961c3a5588e44780
Janae Quinones <janaemassa@gmail.com>
2021-04-03 17:36:47 -0400

    update loading categories

--- src/components/categories.js
+++ src/components/categories.js
@@ -8,13 +8,16 @@ class Categories {
 
     fetchAndLoadCategories() {
         // makes a call to backend API using getCategories method (defined in categories.js)
-        this.adapter.getCategories().then(categories => {
-            // pushes the categories from the API into the categores array defined above
-            categories.forEach(category => this.categories.push(category))
-        })
-        .then(() => {
-            this.renderCategories()
-        })
+        this.adapter
+            .getCategories()
+            .then(categories => {
+                // pushes the categories from the API into the categores array defined above
+                categories.forEach(category => this.categories.push(new Category(category)))
+                console.log(this.categories)
+            })
+            .then(() => {
+                this.renderCategories()
+            })
     }
     renderCategories() {
         // show categories on DOM
---
+++ src/components/category.js
@@ -0,0 +1,7 @@
+class Category {
+    constructor(categoryJSON) {
+        // console.log(categoryJSON)
+        this.id = categoryJSON.id
+        this.name = categoryJSON.name
+    }
+}
