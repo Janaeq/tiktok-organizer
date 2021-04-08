b5d3c6fb48c1ccbc9870e831311d65f6ae0b8b50
Janae Quinones <janaemassa@gmail.com>
2021-04-08 19:55:44 -0400

    add element to constructor

--- src/components/Video.js
+++ src/components/Video.js
@@ -6,35 +6,36 @@ class Video {
         this.embed_html = embed_html
         this.category_id = category_id
         this.thumbnail_url = thumbnail_url
+        this.videoList = document.createElement('ul')
+        this.videoList.setAttribute('style', "list-style-type:none;")
         this.constructor.all.push(this)
     }
-    // static makes it a class method
-    static addEventListeners(videos) {
-        videos.forEach(video => {
-            const addVideoBtn = document.getElementById(`new-${video.category_id}`)
-            addVideoBtn.addEventListener('click', this.removeBtnAndShowForm)
-            const videoForm = document.getElementById(`form-${video.category_id}`)
-            videoForm.addEventListener('submit', videoAPI.createVideo)
-        })
-    }
 
-    static removeBtnAndShowForm() {
-        const videoForm = this.nextElementSibling
-        this.style.display = 'none'
-        videoForm.innerHTML += `<input type="text" name="url" value="" placeholder="TikTok URL" id="vid-${this.id}"/>
-        <input type="submit" name="submit" value="go arrow" class="submit"/>`
-        videoForm.style = ""
-    }
+    // static removeBtnAndShowForm(e) {
+    //     if (e.target.classList.value === 'create-video-btn') {
+    //         const btn = e.target
+    //         const videoForm = btn.nextElementSibling
+    //         const id = parseInt(videoForm.id.split('-')[1])
+    //         btn.style.display = 'none'
+    //         videoForm.innerHTML += `<input type="text" name="url" value="" placeholder="TikTok URL" id="vid-${id}"/>
+    //             <input type="submit" name="submit" value="go arrow" class="submit"/>`
+    //         videoForm.style = ""
+    //         videoForm.addEventListener('submit', videoAPI.createVideo)
+    //     }
+    // }
 
-    static renderVideos(filteredVideos) {
-        filteredVideos.forEach(video => {
-            const videosContainer = document.getElementById(`cat-${video.category_id}`).children[0]
-            videosContainer.innerHTML += `<li style="display: inline;"><img src=${video.thumbnail_url} width="108" height="192"></li>`
-        })
-    }
+    // attachToDOM() {
+    //     const videosContainer = document.getElementById(`cat-${this.category_id}`).appendChild(this.videoList)
+    //     videosContainer.innerHTML += `<li style="display: inline;">
+    //         <img src=${this.thumbnail_url} width="108" height="192">
+    //         <button class="vid-delete-btn" id="video-${this.id}" data-action="delete">DeleteVideo</button></li>`
+    //     // add event listener to display embedded video
+    //     // this.videoList.children[0].firstElementChild add embed event listener to this
+    //     document.getElementById(`video-${this.id}`).addEventListener('click', this.deleteVideo)
+    // }
 
-    attachToDOM() {
-        const videosContainer = document.getElementById(`cat-${this.category_id}`).children[0]
-        videosContainer.innerHTML += `<li style="display: inline;"><img src=${this.thumbnail_url} width="108" height="192"></li>`
-    }
+    // deleteVideo(e) {
+    //     videoAPI.deleteVideo(this.id.split('-')[1])
+    //     this.parentElement.parentElement.remove()
+    // }
 }
