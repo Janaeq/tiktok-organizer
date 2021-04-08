7395e8d8f9c70b04a788a822e04ce363bbf3b6f6
Janae Quinones <janaemassa@gmail.com>
2021-04-08 02:07:04 -0400

    attach to DOM

--- src/components/Video.js
+++ src/components/Video.js
@@ -1,9 +1,9 @@
 class Video {
     static all = []
-    constructor(id, url, thumbnail_url, embedHTML, category_id) {
+    constructor(id, url, thumbnail_url, embed_html, category_id) {
         this.id = id
         this.url = url
-        this.embedHTML = embedHTML
+        this.embed_html = embed_html
         this.category_id = category_id
         this.thumbnail_url = thumbnail_url
         this.constructor.all.push(this)
@@ -11,26 +11,30 @@ class Video {
     // static makes it a class method
     static addEventListeners(videos) {
         videos.forEach(video => {
-            console.log(video.id)
             const addVideoBtn = document.getElementById(`new-${video.category_id}`)
             addVideoBtn.addEventListener('click', this.removeBtnAndShowForm)
+            const videoForm = document.getElementById(`form-${video.category_id}`)
+            videoForm.addEventListener('submit', videoAPI.createVideo)
         })
-        // const videoForm = document.querySelector('.add-video')
-        // videoForm.addEventListener('submit', this.createVideo)
     }
 
     static removeBtnAndShowForm() {
         const videoForm = this.nextElementSibling
         this.style.display = 'none'
-        videoForm.innerHTML += `<input type="text" name="url" value="" placeholder="TikTok URL" id="new-vid-inp"/>
+        videoForm.innerHTML += `<input type="text" name="url" value="" placeholder="TikTok URL" id="vid-${this.id}"/>
         <input type="submit" name="submit" value="go arrow" class="submit"/>`
         videoForm.style = ""
     }
 
     static renderVideos(filteredVideos) {
         filteredVideos.forEach(video => {
-            const videosContainer = document.getElementById(`cat-${video.category_id}`)
-            videosContainer.firstElementChild.innerHTML += `<li style="display: inline;"><img src=${video.thumbnail_url} width="108" height="192"></li>`
+            const videosContainer = document.getElementById(`cat-${video.category_id}`).children[0]
+            videosContainer.innerHTML += `<li style="display: inline;"><img src=${video.thumbnail_url} width="108" height="192"></li>`
         })
+    }
+
+    attachToDOM() {
+        const videosContainer = document.getElementById(`cat-${this.category_id}`).children[0]
+        videosContainer.innerHTML += `<li style="display: inline;"><img src=${this.thumbnail_url} width="108" height="192"></li>`
     }
 }
