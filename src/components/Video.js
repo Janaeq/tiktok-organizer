42ded050e950160d25502b810f0ac67b3f352c32
Janae Quinones <janaemassa@gmail.com>
2021-04-07 23:01:38 -0400

    refactor how videos are rendered, and add event listeners

--- src/components/Video.js
+++ src/components/Video.js
@@ -8,14 +8,29 @@ class Video {
         this.thumbnail_url = thumbnail_url
         this.constructor.all.push(this)
     }
-
-    static renderVideos(category_id) {
-        const filteredVideos = this.all.filter(video => {
-            return video.category_id === category_id
+    // static makes it a class method
+    static addEventListeners(videos) {
+        videos.forEach(video => {
+            console.log(video.id)
+            const addVideoBtn = document.getElementById(`new-${video.category_id}`)
+            addVideoBtn.addEventListener('click', this.removeBtnAndShowForm)
         })
+        // const videoForm = document.querySelector('.add-video')
+        // videoForm.addEventListener('submit', this.createVideo)
+    }
+
+    static removeBtnAndShowForm() {
+        const videoForm = this.nextElementSibling
+        this.style.display = 'none'
+        videoForm.innerHTML += `<input type="text" name="url" value="" placeholder="TikTok URL" id="new-vid-inp"/>
+        <input type="submit" name="submit" value="go arrow" class="submit"/>`
+        videoForm.style = ""
+    }
+
+    static renderVideos(filteredVideos) {
         filteredVideos.forEach(video => {
-            const videosContainer = document.getElementById(`cat-${category_id}`)
-            videosContainer.firstElementChild.innerHTML = `<li style="display: inline;"><img src=${video.thumbnail_url} width="108" height="192"></li>`
+            const videosContainer = document.getElementById(`cat-${video.category_id}`)
+            videosContainer.firstElementChild.innerHTML += `<li style="display: inline;"><img src=${video.thumbnail_url} width="108" height="192"></li>`
         })
     }
 }
